071c0271ff31e028c5e358c91cbd435ddad47a3b
KwonDoYoung <entropy0437@gmail.com>
2023-04-20 12:32:27 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Button from "./Button";
+import styles from "./App.module.css"
+import { useState } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1>Welcome back! </h1>
+      <h1 className={styles.title}>Welcome back! {counter}</h1>
+      <Button text="Click me" onClick={onClick} />
     </div>
   );
 }
