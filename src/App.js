1b3a83f5b1aea1c49deed0f96be0528d7c56fecd
KwonDoYoung <entropy0437@gmail.com>
2023-04-20 14:40:03 +0900

    movie-1

--- src/App.js
+++ src/App.js
@@ -31,29 +31,56 @@ import { useState, useEffect } from "react";
 //   </div >
 // }
 
+// function App() {
+//   const [loading, setLoading] = useState(true)
+//   const [coins, setCoins] = useState([])
+//   useEffect(() => {
+//     fetch("https://api.coinpaprika.com/v1/tickers")
+//       .then((response) => response.json())
+//       .then(((json) => {
+//         setCoins(json);
+//         setLoading(false);
+//       }))
+//   }, [])
+
+//   return <div>
+//     <h1>The Coins!! ({loading ? "" : coins.length})</h1>
+//     {loading ?
+//       <strong>Loading</strong> :
+//       <ul>
+//         {coins.map((coin) => <li key={coin.id}>
+//           {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD
+//         </li>)}
+//       </ul>}
+
+//   </div>
+// }
+
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [coins, setCoins] = useState([])
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then(((json) => {
-        setCoins(json);
-        setLoading(false);
-      }))
-  }, [])
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
+    const json = await response.json()
+    setMovies(json.data.movies);
+    setLoading(false)
+  }
 
+  useEffect(() => { getMovies() }, [])
   return <div>
-    <h1>The Coins!! ({loading ? "" : coins.length})</h1>
-    {loading ?
-      <strong>Loading</strong> :
-      <ul>
-        {coins.map((coin) => <li key={coin.id}>
-          {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD
-        </li>)}
-      </ul>}
-
+    {loading ? <h1>Loading...</h1> :
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img src={movie.medium_cover_image}></img>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g, i) => <li key={i}>{g}</li>)}
+            </ul>
+          </div>
+        ))}
+      </div>}
   </div>
 }
-
 export default App;
